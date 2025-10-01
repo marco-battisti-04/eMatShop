9af3ad1dae17e8479c0e10abdd5db15e7364f7b4
federico-potrich <federico.potrich@mat.tn.it>
2025-10-01 15:26:59 +0200

    [ server ] fix for testing

--- server/usermanager/src/main/java/it/outdoor/usermanager/UsermanagerApplication.java
+++ server/usermanager/src/main/java/it/outdoor/usermanager/UsermanagerApplication.java
@@ -2,8 +2,10 @@ package it.outdoor.usermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsermanagerApplication {
 
 	public static void main(String[] args) {
--- server/usermanager/src/main/java/it/outdoor/usermanager/repositories/UserRepository.java
+++ server/usermanager/src/main/java/it/outdoor/usermanager/repositories/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    List<User> findByCategory(String category);
+    List<User> findByName(String name);
 }
